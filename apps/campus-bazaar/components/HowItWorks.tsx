f9e2a077ebd84b66ae3727b451caf3915eda8661
bedlam343 <jagik22@gmail.com>
2025-12-08 17:00:10 -0800

    make step cards appear just a little faster

--- apps/campus-bazaar/components/HowItWorks.tsx
+++ apps/campus-bazaar/components/HowItWorks.tsx
@@ -43,7 +43,7 @@ const HowItWorks = () => {
     hidden: { opacity: 0 },
     visible: {
       opacity: 1,
-      transition: { staggerChildren: 0.3, delayChildren: 0.2 },
+      transition: { staggerChildren: 0.3, delayChildren: 0 },
     },
   };
 
